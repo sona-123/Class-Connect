40f02f848ec500ffc7aaaca306962178d78dc843
Sonali <sonalibyahut0125@gmail.com>
2024-07-03 02:46:57 +0530

    checkk

--- my-next-app/src/app/firebaseConfig/firebaseConfig.js
+++ my-next-app/src/app/firebaseConfig/firebaseConfig.js
@@ -2,14 +2,15 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyAmVbB9S0f5NE0T1h6Ciy8rHodJyYnPIrY",
+  authDomain: "encryshare-7ab6b.firebaseapp.com",
+  projectId: "encryshare-7ab6b",
+  storageBucket: "encryshare-7ab6b.appspot.com",
+  messagingSenderId: "738236375508",
   appId: process.env.FIREBASE_APPID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  measurementId: "1:738236375508:web:728e6420f3c6ef4a733641"
 };
 
 
