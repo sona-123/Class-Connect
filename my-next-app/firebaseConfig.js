48ba46165ce6dd0a74766b1a9c18e53337d49d0c
Sonali <sonalibyahut0125@gmail.com>
2024-07-02 16:30:59 +0530

    Saved private keys

--- my-next-app/firebaseConfig.js
+++ my-next-app/firebaseConfig.js
@@ -3,15 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyAmVbB9S0f5NE0T1h6Ciy8rHodJyYnPIrY",
-  authDomain: "encryshare-7ab6b.firebaseapp.com",
-  projectId: "encryshare-7ab6b",
-  storageBucket: "encryshare-7ab6b.appspot.com",
-  messagingSenderId: "738236375508",
-  appId: "1:738236375508:web:728e6420f3c6ef4a733641",
-  measurementId: "G-TRE91BENPP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-console.log(firebaseConfig);
+
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
