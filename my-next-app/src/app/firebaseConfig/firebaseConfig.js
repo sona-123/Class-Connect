80743063c72d4afb9015387b89009f9774ac065d
Sonali <sonalibyahut0125@gmail.com>
2024-07-03 02:52:25 +0530

    check

--- my-next-app/src/app/firebaseConfig/firebaseConfig.js
+++ my-next-app/src/app/firebaseConfig/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmVbB9S0f5NE0T1h6Ciy8rHodJyYnPIrY",
-  authDomain: "encryshare-7ab6b.firebaseapp.com",
-  projectId: "encryshare-7ab6b",
-  storageBucket: "encryshare-7ab6b.appspot.com",
-  messagingSenderId: "738236375508",
-  appId: process.env.FIREBASE_APPID,
-  measurementId: "1:738236375508:web:728e6420f3c6ef4a733641"
+    apiKey: "AIzaSyAmVbB9S0f5NE0T1h6Ciy8rHodJyYnPIrY",
+    authDomain: "encryshare-7ab6b.firebaseapp.com",
+    projectId: "encryshare-7ab6b",
+    storageBucket: "encryshare-7ab6b.appspot.com",
+    messagingSenderId: "738236375508",
+    appId: "1:738236375508:web:728e6420f3c6ef4a733641",
+    measurementId: "G-TRE91BENPP"
 };
 
 
