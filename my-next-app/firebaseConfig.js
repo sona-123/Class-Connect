07ccdca4686cfed7b0c13e142efb5e1897abeb45
Sonali <sonalibyahut0125@gmail.com>
2024-07-03 11:25:09 +0530

    Fixed auth/invalid-api-key

--- my-next-app/firebaseConfig.js
+++ my-next-app/firebaseConfig.js
@@ -1,5 +1,5 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
@@ -13,8 +13,9 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export const auth = getAuth(app);
+export { app, db, storage, auth };
