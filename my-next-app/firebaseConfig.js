392da2c2cb70505c41d912347ea15c60ea6c03d8
Sonali <sonalibyahut0125@gmail.com>
2024-07-03 12:09:25 +0530

    testing

--- my-next-app/firebaseConfig.js
+++ my-next-app/firebaseConfig.js
@@ -4,16 +4,16 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-};
+    apiKey: "AIzaSyAmVbB9S0f5NE0T1h6Ciy8rHodJyYnPIrY",
+    authDomain: "encryshare-7ab6b.firebaseapp.com",
+    projectId: "encryshare-7ab6b",
+    storageBucket: "encryshare-7ab6b.appspot.com",
+    messagingSenderId: "738236375508",
+    appId: "1:738236375508:web:728e6420f3c6ef4a733641",
+    measurementId: "G-TRE91BENPP"
+  };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app =  initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const auth = getAuth(app);
