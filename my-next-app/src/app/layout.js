9c7a7caad0a5276c4c77ef826a95b6eed83f4c5d
Sonali <sonalibyahut0125@gmail.com>
2024-07-03 12:14:42 +0530

    added publishable key

--- my-next-app/src/app/layout.js
+++ my-next-app/src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <html lang="en">
       <body className={inter.className}>
 
